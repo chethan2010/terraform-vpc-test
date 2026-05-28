19b3facde45de6de178d4a3d740789320ff364a8
chethan2010 <hemanthkumarsaketi@gmail.com>
2026-05-28 13:22:52 +0530

    expense

--- outputs.tf
+++ outputs.tf
@@ -2,9 +2,9 @@ output "vpc_id" {
   value = module.vpc.vpc_id
 }
 
-output "public_subnet_list" {
-  value = module.vpc.public_subnet_cidrs
-}
+# output "public_subnet_list" {
+#   value = module.vpc.public_subnet_cidrs
+# }
 
 output "igw_id" {
   value = module.vpc.igws_id
